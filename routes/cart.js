008f060e73b22d5b81ab57be86ca35ea02d95e8e
a.ali <a.ali@actrix.co.nz>
2016-05-23 20:27:03 +1200

    Implement checkout form. Move checkout to its own route. Fix some display glitches.

--- routes/cart.js
+++ routes/cart.js
@@ -63,34 +63,4 @@ router.get('/cart/add/:id', function(req, res) {
 
 });
 
-// Get: /checkout
-router.get('/cart/checkout', function(req, res) {
-  checkLogin(req, res);
-
-  var addressSql = "SELECT * FROM addresses where user_id = ?";
-  var addressParams = req.session.user.id;
-  req.db.query(addressSql, addressParams, function(err, addresses) {
-    if (err) {
-      return res.sendStatus(500);
-    }
-
-    var softwareSql = "SELECT SUM(software.price) AS sum FROM carts INNER JOIN software ON software.id = carts.software_id WHERE carts.user_id = ?";
-    var softwareParams = req.session.user.id;
-    req.db.query(softwareSql, softwareParams, function(err, softwarePrice) {
-      if (err) {
-        return res.sendStatus(500);
-      }
-
-      res.render('cart/checkout', {
-        title: 'SWEN303 Project',
-        addresslist: addresses,
-        total: softwarePrice[0].sum,
-      });
-
-    });
-
-  });
-
-});
-
 module.exports = router;
